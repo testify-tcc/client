019a9d8982ef1629e2b4eaee643630c63ee4a8fc
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-08-28 14:54:44 -0300

    chore: setup react-refresher

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
 export function App() {
-  return <div className="app-container">Hello, World!</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="app-container">
+      <h1>Hello, World!</h1>
+      <button
+        onClick={() => {
+          setCounter((oldCounter) => oldCounter + 1);
+        }}
+      >
+        Counter: {counter}
+      </button>
+    </div>
+  );
 }
