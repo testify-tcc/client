259d4635deb16cab53b782b726b77b0db1d3e35a
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-08-28 19:05:27 -0300

    chore: add default colors

--- src/theme.ts
+++ src/theme.ts
@@ -5,9 +5,22 @@ export enum FontFamilies {
   OPEN_SANS = "open-sans",
 }
 
+export enum Colors {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+  WHITE = "white",
+  BLACK = "back",
+}
+
 export const theme = extendTheme({
   fonts: {
     [FontFamilies.COMFORTAA]: `"Comfortaa", cursive`,
     [FontFamilies.OPEN_SANS]: `"Open Sans", sans-serif`,
+  },
+  colors: {
+    [Colors.PRIMARY]: "#35bab8",
+    [Colors.SECONDARY]: "#dc9934",
+    [Colors.WHITE]: "#f7f7f7",
+    [Colors.BLACK]: "#272727",
   },
 });
