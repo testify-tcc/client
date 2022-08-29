0239ebe0aa31868588fa71b288add22389b32ab3
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-08-28 21:11:47 -0300

    chore: add theme to chackra provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import "./index.scss";
 import { App } from "src/App";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom";
+import { theme } from "./theme";
 
 ReactDOM.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
   document.getElementById("root"),
